148ebd900081d596081c70c7c117c9321ec757e5
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2025-03-27 21:07:31 +0100

    * rebuilt docs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     author="Ruman Gerst",
     author_email='ruman.gerst@leibniz-hki.de',
-    classifiers=['License :: OSI Approved :: BSD-2-Clause License', 'Programming Language :: Python :: 3.8'],
+    classifiers=['License :: OSI Approved :: BSD-2-Clause License', 'Programming Language :: Python :: 3.10'],
     description="Provides integration into JIPipe",
     license="BSD-2-Clause license",
     include_package_data=True,
